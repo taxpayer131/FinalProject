12ee3e369c260bc5c254c7d694988b2397bc013e
taxpayer131.Chris <taxpayer131.chris.stefan1844@gmail.com>
2017-12-11 14:26:43 -0800

    Add project files.

---
+++ final_project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(final_project.Startup))]
+namespace final_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
