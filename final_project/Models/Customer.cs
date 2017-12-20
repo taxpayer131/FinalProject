a92d9133f01aea7c8a946ea8085bee379910fcb0
taxpayer131.Chris <taxpayer131.chris.stefan1844@gmail.com>
2017-12-20 14:07:05 -0800

    regular work save

--- final_project/Models/Customer.cs
+++ final_project/Models/Customer.cs
@@ -9,5 +9,7 @@ namespace final_project.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
     }
 }
--- final_project/Models/IdentityModels.cs
+++ final_project/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace final_project.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
